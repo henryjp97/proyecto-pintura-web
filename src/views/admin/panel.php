8285323c52b01a3f8d983653669cdcc949d51415
stephanoJ <stephanosequeiros@gmail.com>
2026-05-18 18:57:08 +0200

    cambio de mayusculas a minusculas

--- src/views/admin/panel.php
+++ src/views/admin/panel.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../../config/conexion.php';
-require_once __DIR__ . '/../../models/AdminModel.php';
+require_once __DIR__ . '/../../models/adminmodel.php';
 require_once __DIR__ . '/../../controllers/AdminController.php';
 
 /* ── 1. Autenticación ── */
