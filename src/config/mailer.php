7ac6a5c20623ea60570a86a2997eced20773b123
stephanoJ <stephanosequeiros@gmail.com>
2026-05-18 19:42:15 +0200

    mailer actualizado con brevo

--- src/config/mailer.php
+++ src/config/mailer.php
@@ -8,12 +8,12 @@ function crearMailer(): PHPMailer {
     $mail = new PHPMailer(true);
 
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
+    $mail->Host       = 'smtp-relay.brevo.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'finishlineheesni@gmail.com';// Crearos en .env MAIL_USER=Tu MAIL        MAIL_PASS=TU CONTRASEÑA (La que tienes que buscar en google(no la que usualmente usas))
-    $mail->Password   = 'hmgtzemohczkblko';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
+    $mail->Username   = 'abb8d3001@smtp-brevo.com';
+    $mail->Password   = 'bskl3t2LkCeBpgp';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
     $mail->setFrom('finishlineheesni@gmail.com', 'FinishLine');
     $mail->CharSet = 'UTF-8';
