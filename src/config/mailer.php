d60ad2f123e51d051868fd69e33f88a70f4a4687
stephanoJ <stephanosequeiros@gmail.com>
2026-05-19 11:25:51 +0200

    Integración Brevo mailer con variable de entorno

--- src/config/mailer.php
+++ src/config/mailer.php
@@ -1,6 +1,6 @@
 <?php
 function enviarCorreo(string $destinatario, string $asunto, string $cuerpoHtml): bool {
-    $apiKey = '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';
+    $apiKey = $_ENV['BREVO_API_KEY'];
 
     $datos = [
         'sender'     => ['name' => 'FinishLine', 'email' => 'finishlineheesni@gmail.com'],
